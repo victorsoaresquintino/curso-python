1146a60f84089b8d2146e69109b7fd694e5a548f
victorsoaresquintino <victorsoaresquintino@gmail.com>
2022-03-17 07:38:31 -0300

    primeica alteração

---
+++ aula-06.py
@@ -0,0 +1,8 @@
+# n1=int(input('Digite um valor: '))
+# n2=int(input('Digite outro numero: '))
+# r=n1+n2
+# print('o resultado de {} mais {} é {}'.format(n1,n2,r))
+import inspect
+
+x = input('digite: ')
+print(x)
